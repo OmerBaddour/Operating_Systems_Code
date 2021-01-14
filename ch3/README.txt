097ef350b0be69be1db2dda6dc1af3713c90478a
OmerBaddour <omer.m.baddour@gmail.com>
2021-01-14 09:00:23 -0700

    simple-shell part I done

--- ch3/README.txt
+++ ch3/README.txt
@@ -1,19 +1,23 @@
-shm-posix-producer and shm-posix-consumer:
+shm-posix-producer.c and shm-posix-consumer.c:
 
-Test the writing to and reading of shared memory. First run fig3-17 in one
-window, and then run fig3-18 in another window, to print the written text
-to stdout.
+Test the writing to and reading of shared memory. First run shm-posix-producer.c
+in one window, and then run shm-posix-consumer.c in another window, to print the
+written text to stdout.
+
+$ make shm
 
 --------------------------------------------------------------------------------
 
-unix_pipe:
+unix_pipe.c:
 
 Test the creation of an ordinary pipe, which is used to write data to the child 
 process from the parent process.
 
+$ make unix_pipe
+
 --------------------------------------------------------------------------------
 
-fig3-30, multi-fork, fig3-32, -33, -34, -35:
+fig3-30.c, multi-fork.c, fig3-32.c, -33.c, -34.c, -35.c:
 
 Test fork() stuff.
 Note: n sequential calls to fork() results in 2^n processes
@@ -21,3 +25,13 @@ Note: a for loop which repeats n times, and calls fork() in each iteration,
 results in 2^n processes
 Explanation: fork() doubles the number of processes present.
 
+$ make fig3-30 fig3-32 ...
+
+--------------------------------------------------------------------------------
+
+simple-shell.c
+
+Simple shell program. Slightly modified from textbook implementation: deleted
+should_run variable, and replaced with an infinite loop with a break case.
+
+$ make simple-shell
--- ch3/simple-shell.c
+++ ch3/simple-shell.c
@@ -7,24 +7,84 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 
-#define MAX_LINE		80 /* 80 chars per line, per command */
+#define MAX_LINE 80 /* 80 chars per line, per command */
 
 int main(void)
 {
-	char *args[MAX_LINE/2 + 1];	/* command line (of 80) has max of 40 arguments */
-    	int should_run = 1;
+	char *args[MAX_LINE/2 + 1]; /* command line (of 80) has max of 40 arguments */
 		
-    	while (should_run){   
+    	while (1) {   
+		/* prompt user */
         	printf("osh>");
         	fflush(stdout);
         
-        	/**
-         	 * After reading user input, the steps are:
-         	 * (1) fork a child process
-         	 * (2) the child process will invoke execvp()
-         	 * (3) if command included &, parent will invoke wait()
-         	 */
+		/* read user input */
+		char input[MAX_LINE];
+		fgets(input, MAX_LINE, stdin); 
+
+		/* process user input 
+		 * replace spaces with null terminators, and populate args with
+		 * pointers to the beginning of strings in input[] */
+		int cur_arg = 0;
+		int new_arg = 1;
+		int len_input = strlen(input);
+		for (int i = 0; i < len_input; i++) { 
+			if (isspace(input[i]) == 0) { /* not whitespace */
+				if (new_arg == 1) {
+					args[cur_arg] = &input[i];
+					cur_arg++;
+					new_arg = 0;
+				}
+			}
+			else { 
+				input[i] = 0;
+				new_arg = 1;
+			}
+		}
+
+		/* zero out remainder of args */
+		for (int i = cur_arg; i < MAX_LINE/2 + 1; i++) {
+			args[i] = 0;
+		}
+
+		/* check special cases:
+		 * - were any args enterred?
+		 * - is exit enterred?
+		 * - is last argument &? */
+		int background = 0;
+		if (cur_arg == 0) { 
+			continue; /* no args enterred */
+		}
+		else if (cur_arg == 1 && strcmp(args[cur_arg-1], "exit") == 0) {
+			break; /* exit */
+		}
+		else if (strcmp(args[cur_arg-1], "&") == 0) {
+			background = 1;
+			args[cur_arg-1] = 0; /* trailing & should not be passed to execvp */
+		}
+
+		pid_t pid = fork();
+
+		if (pid < 0) {
+			fprintf(stderr, "Fork failed");
+			exit(1);
+		}
+
+		if (pid > 0) { /* parent process */
+			if (background == 0) { /* wait */
+				int status;
+				wait(&status);
+			} 
+		}
+		else { /* child process */
+			execvp(args[0], args);
+		}
+
     	}
     
 	return 0;
