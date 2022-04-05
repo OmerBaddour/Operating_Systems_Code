6c3e4b2f322582f4f95d954ba903b7148081252f
OmerBaddour <omer.m.baddour@gmail.com>
2022-04-04 22:03:32 -0600

    monitor version of file

---
+++ ch05/prog-probs/resource_manager_monitor.h
@@ -0,0 +1,17 @@
+#include <semaphore.h>
+
+#define MAX_RESOURCES 5
+
+/* initialize the monitor */
+void init_monitor(void);
+
+/* decrease the number of resources
+ * now does not return the number of resources */
+void decrease_count(int);
+
+/* increase the number of resources */
+int increase_count(int);
+
+/* clean up the monitor */
+void free_monitor(void);
+
