a548176742606a24dee19436c7c0db795604f381
OmerBaddour <omer.m.baddour@gmail.com>
2022-04-13 00:11:53 -0600

    completed code

--- ch05/prog-projs/producer_and_consumer.c
+++ ch05/prog-projs/producer_and_consumer.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h> /* required for rand() */
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
 #include <stdbool.h>
 #include "buffer.h"
+#include "producer_and_consumer.h"
 
 void *producer(void *param) {
+	srand(time(NULL));
+
 	buffer_item item;
-	
+
 	while (true) {	
 		/* sleep for random period of time */
-		sleep(1);
+		sleep((rand() % MAX_SLEEP_TIME) + 1);
 
 		/* generate random number */
 		item = rand();
 
 		if (insert_item(item)) {
-			fprintf(stderr, "report error condition");
+			fprintf(stderr, "report error condition\n");
 		}
 		else {
 			printf("producer produced %d\n", item);
@@ -23,14 +28,16 @@ void *producer(void *param) {
 }
 
 void *consumer(void *param) {
+	srand(time(NULL));
+
 	buffer_item item;
 	
 	while (true) {	
 		/* sleep for random period of time */
-		sleep(1);
+		sleep((rand() % MAX_SLEEP_TIME) + 1);
 
 		if (remove_item(&item)) {
-			fprintf(stderr, "report error condition");
+			fprintf(stderr, "report error condition\n");
 		}
 		else {
 			printf("consumer consumed %d\n", item);
